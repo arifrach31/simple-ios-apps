c1fb27be1de76da6ed61d22c53d3c958724627ff
telkomdev-arifrach31 <arifrach31@gmail.com>
2020-05-28 19:04:10 +0700

    Initial Commit

---
+++ Androidversion/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Androidversion
+//
+//  Created by ArifRachman on 28/05/20.
+//  Copyright © 2020 ArifRachman. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
